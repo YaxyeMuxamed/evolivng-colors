bca329c1ff4a9ccd3fec19b76bf32df7bc1b4c5f
Yaxye Muxamed <yaxye328@gmail.com>
2024-04-21 23:03:23 -0700

    updated and fixed bugs in automata class

--- automata.js
+++ automata.js
@@ -1,50 +1,51 @@
 class Automata {
-	constructor() {
-		this.plants = [];
-		this.length = 100;
+    constructor() {
+        this.length = 100;
         this.width = 100;
-        this.plants = Array.from({length: this.length}, () => 
-        Array.from({width: this.width}, () => null));
-	}
-	
-	
-	clearPlants() {
-        let i = 0;
-        while (i < this.width) {
-            let j = 0;
-            while (j < this.length) {
+        this.plants = Array.from({length: this.width}, () => new Array(this.length).fill(null));
+    }
+    
+    clearPlants() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.length; j++) {
                 this.plants[i][j] = null;
-                j++;
             }
-            i++;
         }
     }
 
-	addPlant() {
-		let i = randomInt(this.width);
-		let j = randomInt(this.length);
-		let color = randomInt(360);
-		this.plants[i][j] = new Plant({hue: color, x: i, y: j}, this)
-	}
+    addPlant() {
+        let i = this.randomInt(this.width);
+        let j = this.randomInt(this.length);
+        if (!this.plants[i][j]) { // Ensure we don't overwrite an existing plant
+            let color = this.randomInt(360);
+            this.plants[i][j] = new Plant({hue: color, x: i, y: j}, this);
+        }
+    }
 
-	
-	update() {
+    update() {
         this.plants.forEach((row, x) => {
             row.forEach((plant, y) => {
-                plant?.update();
-                if (Math.random() < 0.001) {
-                    this.plants[x][y] = null;
+                if (plant) {
+                    plant.update();
+                    if (Math.random() < 0.001) {
+                        this.plants[x][y] = null; // Randomly clear plants with a very small probability
+                    }
                 }
             });
         });
     }
 
-	draw(ctx) {
+    draw(ctx) {
         this.plants.forEach((row, x) => {
             row.forEach((plant, y) => {
-                plant?.draw(ctx);
+                if (plant) {
+                    plant.draw(ctx);
+                }
             });
         });
     }
-	
-};
+    
+    randomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+}
